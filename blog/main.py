cd81ecca25dda139adfadbde5b78021e1b04dd46
Bahna-Darius <emanuelbahna@gmail.com>
2023-09-18 23:25:30 +0300

    Modification

--- blog/main.py
+++ blog/main.py
@@ -29,14 +29,16 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}")
 def delete_blog(id, db: Session = Depends(get_db)):
-    (db.query(models.Blog).filter(models.Blog.id ==
-                                  id).delete(synchronize_session=False))
-    db.commit()
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
 
-    messaje = HTTPException(status_code=status.HTTP_200_OK,
-                    detail=f"Blog with id:{id} has been successfully deleted")
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found!")
+    else:
+        blog.delete(synchronize_session=False)
+        db.commit()
 
-    return {"data": messaje}
+    return {"data": "delete blog success"}
 
 
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
