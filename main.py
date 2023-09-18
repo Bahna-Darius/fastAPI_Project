ebc190b31790d1cb605323bbf856eef59a5674e0
Bahna-Darius <emanuelbahna@gmail.com>
2023-09-18 23:16:16 +0300

    add class and modification

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from typing import Optional
-from blog222.body_request import Blog
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
 @app.get("/blog")
-async def root(limit=10, published: bool = True, sort: Optional[str] = None):
+def root(limit=10, published: bool = True, sort: Optional[str] = None):
     #only get 10 published blogs
     if published:
         return {"data": f"{limit} published blogs from the DB"}
@@ -16,20 +16,26 @@ async def root(limit=10, published: bool = True, sort: Optional[str] = None):
 
 
 @app.get("/blog/unpublished")
-async def unpublished():
+def unpublished():
     return {"data": {"message": "all unpublished blogs"}}
 
 
 @app.get("/blog/{id}")      #fetch blog with id = id
-async def show(id: int):
+def show(id: int):
     return {"data": {"message": f"{id}"}}
 
 
 @app.get("/blog/{id}/comments")     #fetch comments of blog with id = id
-async def comments(id):
+def comments(id):
     return {"data": {"1", "2"}}
 
 
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
 @app.post("/blog")
-async def create_blog(request: Blog):
+def create_blog(request: Blog):
     return {"data": f"Blog is created with title as {request.title}"}
