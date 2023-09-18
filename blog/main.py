81e2b426abfd3127098d97494bbbde9ee832972a
Bahna-Darius <emanuelbahna@gmail.com>
2023-09-18 18:05:35 +0300

    Addition of new API and status_code

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status, Response
 import schemas, models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-@app.post("/blogv2")
-def create(request: schemas.Blog):
-    return request
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close_all()
+
+
+#create new blog in db
+@app.post("/blogv2", status_code=status.HTTP_201_CREATED) #status_code add
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get("/blog")
+def all_blogs_return(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+def show(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": f"Blog with the id {id} is not available"}
+
+    return blog
+
+
